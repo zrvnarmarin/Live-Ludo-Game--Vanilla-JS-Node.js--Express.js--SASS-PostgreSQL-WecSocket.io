e88eb2a453924c0a6d0055f978c829806c254d70
zrvnarmarin <76marin76@gmail.com>
2022-09-21 04:07:14 +0200

    code cleanup

--- utillityFunctions.js
+++ utillityFunctions.js
@@ -1,17 +1,9 @@
 // Get origin(central) point of any HTML element
 export const getElementOriginPoint = (element) => {
-    const elementRects = [...element.getClientRects()]
-    let elementOriginPoint = {}
-
-    elementRects.forEach(rectElement => {
-        const { top, right, bottom, left } = rectElement
-        elementOriginPoint = {
-            x: (left + right) / 2,
-            y: (top + bottom) / 2,
-        }
-    })
-
-    return elementOriginPoint
+    return {
+        x: element.clientWidth / 2,
+        y: element.clientHeight / 2
+    }
 }
 
 // Get origin(central) top and left point of any HTML element
