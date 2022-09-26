e7c30ec023b805afcb5c5f870252d804aa7c2cc6
zrvnarmarin <76marin76@gmail.com>
2022-09-26 22:50:26 +0200

    determining middle positions for player start ring

--- dice.js
+++ dice.js
@@ -1,2 +1,43 @@
-const diceNumbers = [1, 2, 3, 4, 5, 6]
+import {getRandomIntFromIntervals} from './utillityFunctions.js'
 
+// DOM selectors
+const dice = document.querySelector('.dice')
+const diceRollButton = document.querySelector('.dice-number-button')
+
+function rotateDiceSide(randomNumber) {
+    if (randomNumber === '1') {
+        dice.style.transform = 'rotateY(360deg)'
+    } 
+    else if (randomNumber === '2') {
+        dice.style.transform = 'rotateX(180deg)'
+    } 
+    else if (randomNumber === '3') {
+        dice.style.transform = 'rotateY(270deg)'
+    } 
+    else if (randomNumber === '4') {
+        dice.style.transform = 'rotateY(90deg)'
+    } 
+    else if (randomNumber === '5') {
+        dice.style.transform = 'rotateX(270deg)'
+    } 
+    else if (randomNumber === '6') {
+        dice.style.transform = 'rotateX(90deg)'
+    }
+}
+
+// Dice rotating to show side that contains exact generated random number
+diceRollButton.addEventListener('click', () => {
+    const diceNumber = getRandomIntFromIntervals(1, 6).toString()
+    console.log(diceNumber)
+
+    // Trigger animation on button click
+    dice.classList.add('rotate-dice')
+
+    // Remove animation after 1 second
+    setTimeout(() => {
+        dice.classList.remove('rotate-dice')
+    }, 1000)
+
+    rotateDiceSide(diceNumber)
+
+})
